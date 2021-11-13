9f355486d59b4abd3ade4c78887b558bcecb1d7c
danipe95 <90843910+danipe95@users.noreply.github.com>
2021-11-12 20:53:17 -0500

    Actualizaci[on back end botones funcionando, creacion de formulario login

---
+++ app/src/main/java/com/example/job1/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.job1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLogout(botonlogout:View) {
+        val intento = Intent(this, MainActivity::class.java)
+        startActivity(intento)
+    }
+}
--- app/src/main/java/com/example/job1/MainActivity.kt
+++ app/src/main/java/com/example/job1/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.job1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private var edtUsername: EditText? = null
+    private var edtPassword: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtUsername = findViewById(R.id.edtUsername)
+        edtPassword = findViewById(R.id.edtPassword)
+
+    }
+
+
+    fun onLogin(botonLogin: View) {
+        if (edtUsername!!.text.toString() == "lfa@email.com") {
+            if (edtPassword!!.text.toString() == "1234") {
+                val intento = Intent(this, LoginActivity::class.java)
+                startActivity(intento)
+            }
+        }
+    }
+
+    fun onRegister(botonRegister: View) {
+        val intento = Intent(this, registro::class.java)
+        startActivity(intento)
+
+
     }
 }
