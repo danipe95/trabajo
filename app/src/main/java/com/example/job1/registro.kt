c927db13a95d0b21abc8d9208c0713b64e94fd30
[Ameliacmi] <[crissmuigle30@hotmail.com]>
2021-11-18 19:15:49 -0500

    cambio en string

--- app/src/main/java/com/example/job1/registro.kt
+++ app/src/main/java/com/example/job1/registro.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class registro : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
     }
 }
