4616a9f6a6b85130a42b0dad6ab740e8aed838c2
[Ameliacmi] <[crissmuigle30@hotmail.com]>
2021-11-18 21:09:16 -0500

    errores corregidos

---
+++ app/src/main/java/com/example/job1/cats.kt
@@ -0,0 +1,11 @@
+package com.example.job1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cats : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cats)
+    }
+}
---
+++ app/src/main/java/com/example/job1/products.kt
@@ -0,0 +1,11 @@
+package com.example.job1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class products : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
