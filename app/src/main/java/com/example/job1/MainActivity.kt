3b7241908500190b5252303235b6c1f15298f70e
danipe95 <90843910+danipe95@users.noreply.github.com>
2021-11-22 21:48:49 -0500

    Actualizacion backend registro google

--- app/src/main/java/com/example/job1/MainActivity.kt
+++ app/src/main/java/com/example/job1/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.job1
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.android.gms.tasks.Task
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE,
+    FACEBOOK
+}
 
 class MainActivity : AppCompatActivity() {
     private var editUsername: EditText? = null
@@ -20,13 +31,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         /*setSupportActionBar(findViewById(R.id.my_toolbar))*/
 
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+
+        bundle.putString(
+            resources.getString(R.string.fire_base),
+            resources.getString(R.string.fire_base_message)
+        )
+        analytics.logEvent(resources.getString(R.string.mersh), bundle)
+
         editUsername = findViewById(R.id.edtUsername)
         editPassword = findViewById(R.id.edtPassword)
+
+        title = resources.getString(R.string.txt_Login)
+
+        session()
 
     }
 
 
-    fun onLogin(botonLogin: View) {
+    /*fun onLogin(botonLogin: View) {
         if (editUsername!!.text.toString() == "lfa@email.com") {
             if (editPassword!!.text.toString() == "1234") {
                 val intento = Intent(this, LoginActivity::class.java)
@@ -34,26 +58,91 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
+    */
 
     fun onRegister(botonRegister: View) {
         val intento = Intent(this, registro::class.java)
-            startActivity(intento)
+        startActivity(intento)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        val prefs =
+            getSharedPreferences(
+                resources.getString(R.string.preds_file),
+                Context.MODE_PRIVATE
+            )
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email == null && provider == null) {
+            var loginLayout = findViewById<LinearLayout>(R.id.loginLayout);
+            loginLayout.visibility = View.VISIBLE
+        }
+    }
+
+
+    private fun session() {
+        val prefs =
+            getSharedPreferences(
+                resources.getString(R.string.preds_file),
+                Context.MODE_PRIVATE
+            )
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null) {
+            var loginLayout = findViewById<LinearLayout>(R.id.loginLayout);
+            loginLayout.visibility = View.INVISIBLE
+
+            showHome(email, ProviderType.valueOf(provider))
         }
 
-    /*override fun onStart() {...}
-    private fun session() {...}
+    }
 
-    fun onLogin(view:View){
+
+    fun onLogin(view: View) {
         var username = editUsername!!.text.toString();
         var password = editPassword!!.text.toString();
 
         if (username.isNotEmpty() && password.isNotEmpty()) {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(username, password)
-                .addOnCompleteListener { Task<AuthResult>
-                }*/
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(username, ProviderType.BASIC)
+                    } else {
+                        getToast(resources.getString(R.string.error_auth));
+                    }
+                }
+
+        } else {
+            getToast(resources.getString(R.string.error_login));
         }
 
+    }
+
+    private fun showHome(username: String, provider: ProviderType) {
+
+        val homeIntent = Intent(this, LoginActivity::class.java).apply {
+            putExtra("email", username)
+            putExtra("provider", provider.toString())
+        }
+
+        startActivity(homeIntent)
+
+        getToast(resources.getString(R.string.txt_welcome));
+    }
+
+    private fun getToast(message: String) {
+        Toast.makeText(
+            applicationContext,
+            message,
+            Toast.LENGTH_LONG
+        ).show();
+    }
+}
 
 
 
