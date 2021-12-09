8836cb3a463f1c19eb0a27c3487aa578b2298459
danipe95 <90843910+danipe95@users.noreply.github.com>
2021-12-08 21:54:25 -0500

    Database con errores, adaptador producto

---
+++ app/src/main/java/com/example/job1/ProductEntity.kt
@@ -0,0 +1,11 @@
+package com.example.job1
+
+public class ProductEntity(imagen:String,title:String,cost:String,category:String,seller:String,id:String,average:Double) {
+    public var  imagen:String=imagen;
+    public var  title:String=title;
+    public var  cost:String=cost;
+    public var  category:String=category;
+    public var  seller:String=seller;
+    public var  id:String=id;
+    public var average:Double=average;
+}
